e3988e60288651be0d96e271fa4b077c2ec79087
jinzhengshuai <46102079+jinzhengshuai@users.noreply.github.com>
2021-07-23 16:20:36 +0800

    Add files via upload

---
+++ maven-plugins/src/test/java/com/geoway/mavenplugins/MavenPluginsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoway.mavenplugins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPluginsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
